8d9e72bd11cc2ab63426edf8f2598a5930a6048e
Abraham Castiello <abrahamcastiellobarbas@gmail.com>
2023-10-31 18:09:19 +0100

    Lab solved

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const app = express() // <= nuestro servidor
+const express = require("express");
+const app = express(); // <= nuestro servidor
 
 app.use(express.static("public"));
 
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/views/homepage.html");
+});
 
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/aboutpage.html");
+});
 
+app.get("/jobs", (req, res) => {
+  res.sendFile(__dirname + "/views/jobpage.html");
+});
 
+app.get("/galery", (req, res) => {
+  res.sendFile(__dirname + "/views/fotogalery.html");
+});
 
-const port = 3000
+const port = 3000;
 
-
-app.listen(port, ()=> {
-    console.log(`escuchando en el puerto ${port}`)
-})
+app.listen(port, () => {
+  console.log(`escuchando en el puerto ${port}`);
+});
